3b60913e87314ab0a91515ca3f052f92941370c2
Namia-kair <kmy258855@outlook.com>
2026-03-30 18:40:27 +0800

    Add files via upload

---
+++ luci-app-ipv6set/luasrc/controller/ipv6set.lua
@@ -0,0 +1,11 @@
+module("luci.controller.ipv6set", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/ipv6set") then
+		return
+	end
+
+	entry({"admin", "network", "ipv6set"}, cbi("ipv6set"), _("IPv6 Settings"), 100).dependent = true
+end
+
+
