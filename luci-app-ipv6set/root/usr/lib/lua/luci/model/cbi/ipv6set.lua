96570cbd894471fef88f0cddd8fdbd0b4b70407a
Namia-kair <kmy258855@outlook.com>
2026-03-30 15:14:38 +0800

    Update ipv6set.lua

--- luci-app-ipv6set/root/usr/lib/lua/luci/model/cbi/ipv6set.lua
+++ luci-app-ipv6set/root/usr/lib/lua/luci/model/cbi/ipv6set.lua
@@ -13,22 +13,27 @@ o = s:option(ListValue, "mode", translate("IPv6 Mode"))
 o:value("0", translate("Disable IPv6 NDP"))
 o:value("1", translate("NDP Native Mode"))
 o:value("2", translate("DHCPv6 Relay Mode"))
+-- o:value("3", translate("NAT66 Mode"))
 o.default = 1
 
 o6 = s:option(ListValue, "wan6zone", translate("Upstream WAN6 Zone"))
 o6.default = "wan6"
 if dump then
-    local i, v
-    for i, v in ipairs(dump.interface) do
-        if v.interface ~= "loopback" then
-            o6:value(v.interface)
+        local i, v
+        for i, v in ipairs(dump.interface) do
+                if v.interface ~= "loopback" then
+                        o6:value(v.interface)
+                end
         end
-    end
 end
 o6.rmempty = false
 
 o = s:option(Flag, "v6dns", translate("Enable IPv6 DNS (AAAA) Lookup"))
 o.default = 1
 o.description = translate("IPv6 DNS (AAAA)")
+
+-- o = s:option(Flag, "v6nat", translate("Enable IPv6 NAT66"))
+-- o.default = 0
+-- o.description = translate("IPv6-to-IPv6 Network Address Translation")
 
 return m
