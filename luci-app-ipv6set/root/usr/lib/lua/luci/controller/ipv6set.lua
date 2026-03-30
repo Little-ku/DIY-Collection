ef4d39c7345c67766187cc36ef6e75683efe27b7
Namia-kair <kmy258855@outlook.com>
2026-03-30 15:14:54 +0800

    Update ipv6set.lua

--- luci-app-ipv6set/root/usr/lib/lua/luci/controller/ipv6set.lua
+++ luci-app-ipv6set/root/usr/lib/lua/luci/controller/ipv6set.lua
@@ -1,9 +1,9 @@
 module("luci.controller.ipv6set", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/ipv6set") then
-        return
-    end
+        if not nixio.fs.access("/etc/config/ipv6set") then
+                return
+        end
 
-    entry({"admin", "network", "ipv6set"}, cbi("ipv6set"), _("IPv6 Settings"), 10).dependent = true
+        entry({"admin", "network", "ipv6set"}, cbi("ipv6set"), _("IPv6 Settings"), 100).dependent = true
 end
